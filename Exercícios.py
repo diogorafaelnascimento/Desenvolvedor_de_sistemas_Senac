22654d43dac141798df95fb2464ed2152e957d01
Diogo Nascimento <diogo62210966@alunos.sc.senac.br>
2026-03-10 21:38:09 -0300

    Atualização

--- Exercícios.py
+++ Exercícios.py
@@ -1,1 +1,5 @@
-print('olá mundo')
+frutas = ['maçã', 'banana', 'uva', 'melancia']
+numeros = [1, 2, 3, 4]
+print(f'Primeira fruta: {frutas[0]}')
+print(f'Última fruta: {frutas[-1]}')
+frutas[1] = 'banana-nanica'
